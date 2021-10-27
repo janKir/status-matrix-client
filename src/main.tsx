e81b09650ab2d4c414b47fe25e088691649bdb20
janKir <9143877+janKir@users.noreply.github.com>
2021-10-28 01:10:47 +0200

    setup graphql

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_GRAPHQL_URL as string,
+  cache: new InMemoryCache()
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+      </ApolloProvider>
   </React.StrictMode>,
   document.querySelector('#root'),
 );
+
