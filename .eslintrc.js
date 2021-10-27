20e48dc3d44772f727cd31f50ae579a01f4b6a96
janKir <9143877+janKir@users.noreply.github.com>
2021-10-28 00:28:48 +0200

    initial commit
    based on https://github.com/janKir/vite-reactts-eslint-airbnb-jest

---
+++ .eslintrc.js
@@ -0,0 +1,75 @@
+// based on https://github.com/iamturns/create-exposed-app/blob/master/.eslintrc.js
+module.exports = {
+  parser: "@typescript-eslint/parser", // allows to lint typescript
+  plugins: [
+    "@typescript-eslint", // allows for TypeScript-specific linting rules to run.
+    "eslint-comments", //Additional ESLint rules for ESLint directive comments (e.g. //eslint-disable-line).
+    "jest", // rules specific for testing with jest
+    "unicorn", // Various awesome ESLint rules 
+    "prettier", //Runs Prettier as an ESLint rule and reports differences as individual ESLint issues.
+    "deprecation", // reports usage of deprecated code
+  ],
+  extends: [
+    "airbnb-typescript", // use airbnb ruleset for typescript as base
+    "airbnb/hooks", // add lint rules for use of react hooks
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended", // use recommended jest rules
+    "plugin:unicorn/recommended", // use recommended rules of unicorn ruleset
+    "prettier", // eslint-config-prettier Turns off all rules that are unnecessary or might conflict with Prettier.
+    "plugin:prettier/recommended", // extend eslint-config-prettier rules
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    jsx: true,
+    sourceType: "module",
+    useJSXTextNode: true,
+    project: "./tsconfig.json",
+  },
+
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  ignorePatterns: [".cache/**/*", "dist/**/*", ".git/**/*", "node_modules/**/*", ".*.js", "jest.*.js", "jest.*.ts", "vite.config.ts"],
+  rules: {  
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
+    // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
+    "react/jsx-filename-extension": "off",
+    "react/jsx-key": "warn",
+    // Use function hoisting to improve code readability
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true },
+    ],
+    // Makes no sense to allow type inferrence for expression parameters, but require typing the response
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      { allowExpressions: true, allowTypedFunctionExpressions: true },
+    ],
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: false, variables: false, typedefs: false },
+    ],
+    // Common abbreviations are known and readable
+    "unicorn/prevent-abbreviations": "off",
+    "@typescript-eslint/no-implied-eval": "off",
+    "@typescript-eslint/no-throw-literal": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "consistent-return": "off",
+    "jest/expect-expect": ["error", { assertFunctionNames: ["expect*"] }],
+    "react/prop-types": "off",
+    "eslint-comments/no-duplicate-disable": "error",
+    "eslint-comments/no-unlimited-disable": "error",
+    "eslint-comments/no-unused-enable": "error",
+    "unicorn/filename-case": "off",
+    "unicorn/no-reduce": "off",
+    "unicorn/no-useless-undefined": "off",
+    "deprecation/deprecation": "warn", // will report all deprecated code
+    "jsx-a11y/label-has-associated-control": ["error", {
+      assert: "either"
+    }]
+  },
+};
---
+++ README.md
@@ -0,0 +1,35 @@
+# Vite + React + Typescript + Eslint (including Airbnb + Prettier) + Jest + Linaria
+
+A starter for React with Typescript with the fast Vite and all static code testing with Eslint and formatting with Prettier.
+
+## Installation
+
+```
+npx degit janKir/vite-reactts-eslint-airbnb-jest #main my-project
+```
+
+# Includes
+
+## Typescript + React + Jest
+
+Latest Typescript (4.4), React (17.0), and Jest (27.3) Versions
+
+## ESLint Prettier
+
+Configures ESLint with a bunch of useful linting rules:
+
+- AirBnB Typescript recommended rules
+- Prettier formatting
+- Jest recommended rules
+- Unicorn recommended rules
+- ESLint comments
+- deprecation warnings
+
+## Vite
+
+Vite (French word for "quick", pronounced /vit/, like "veet") is a build tool that aims to provide a faster and leaner development experience for modern web projects.
+
+## Linaria
+
+ Zero-runtime CSS in JS library. 
+
